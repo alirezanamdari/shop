9cd0b7c6f481e89185cfdf3552d7a88b4643fada
alirezanamdari <alireza.namdare@gmail.com>
2021-02-22 18:00:01 +0330

    web templates added added to the project.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.home');
 });
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+
